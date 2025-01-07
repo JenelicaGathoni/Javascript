83f311c5e1f708157bfbb4ec74f8e2b0c4b190af
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-01-07 11:12:12 +0300

    Learning how to declare variables and create functions

---
+++ index.js
@@ -0,0 +1,9 @@
+//creating a function that squares a given no
+
+function square(number) {
+  return number * number;
+}
+console.log(square(5));
+// Declaring an array and accessing the values inside
+let favoriteColors = ["Red ", " Yellow", " Blue", "black"];
+console.log(" Jenelle's favorite color is" + favoriteColors[2]);
